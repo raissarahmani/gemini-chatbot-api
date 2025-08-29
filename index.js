cfe10f7fb304cd221190dea1df8759706d64036c
rrhmn <raissarahmani18@gmail.com>
2025-08-29 19:59:19 +0700

    feat: api

---
+++ index.js
@@ -0,0 +1,46 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import { GoogleGenAI } from '@google/genai'
+
+const app = express()
+const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY})
+const GEMINI_MODEL = 'gemini-2.5-flash'
+
+app.use(cors())
+app.use(express.json())
+
+const PORT = 9000
+app.listen(PORT, () => console.log(`Server ready on http://localhost:${PORT}`))
+
+function extractText(resp) {
+    try {
+        const text = 
+        resp?.response?.candidates?.[0]?.content?.parts?.[0]?.text ??
+        resp?.candidates?.[0]?.content?.parts?.[0]?.text ??
+        resp?.response?.candidates?.[0]?.content?.text
+
+        return text ?? JSON.stringify(resp, null, 2)
+    } catch (err) {
+        console.error("Error extracting text", err)
+        return JSON.stringify(resp, null, 2)
+    }
+}
+
+app.post('/api/chat', async(req, res) => {
+    try {
+        const { messages } = req.body
+        if (!Array.isArray(messages)) throw new Error("messages must be an array")
+        const contents = messages.map(msg => ({
+            role: msg.role,
+            parts: [{ text: msg.content }]
+        }))
+        const resp = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents
+        })
+        res.json({ result: extractText(resp) })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
